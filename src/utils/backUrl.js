14a6d74767908481d16c6c90d47cb9bcd3a096ce
MlleJea <jeanne.gruson.jg@gmail.com>
2025-07-06 14:04:02 +0200

    back url

--- src/utils/backUrl.js
+++ src/utils/backUrl.js
@@ -1,10 +1,11 @@
-/* export function getBackUrl(){
-    return "http://34.155.195.33:8080/api/rest";
-} */
-
+export function getBackUrl(){
+    return "http://34.1.11.179:8080/api/rest";
+} 
+/*
      export function getBackUrl(){
     return "http://127.0.0.1:8080/api/rest";
  } 
 //  export function getBackUrl(){
 // return "http://192.168.168.187:8080/api/rest";
 // }
+*/
